17cfa0991a1e824d6a6f0f3a4137653fe906f57d
Kevin Wei <kevinwei@mirle.com.tw>
2022-08-02 10:14:26 +0800

    加入AOP-TeaceMethodAspect，用於追蹤Function的計算時間

--- ScriptControl/Common/AOP/TeaceMethodAspectAttribute.cs
+++ ScriptControl/Common/AOP/TeaceMethodAspectAttribute.cs
@@ -26,21 +26,11 @@ namespace com.mirle.ibg3k0.sc.Common.AOP
 
         NLog.Logger logger = LogManager.GetLogger("AOP_MethodExecuteInfo");
         StopWatchPool stopWatchPool = new StopWatchPool();
-        ConcurrentDictionary<string, Stopwatch> StopWatchDictionary { get; set; } = new ConcurrentDictionary<string, Stopwatch>();
 
-        //const string CALL_CONTEXT_KEY_STOPWATCH = "CALL_CONTEXT_KEY_STOPWATCH";
         [Advice(Kind.Before, Targets = Target.Method)]
         public void Before([Argument(Source.Name)] string name, [Argument(Source.Metadata)] System.Reflection.MethodBase methodBase, [Argument(Source.Arguments)] object[] arguments)
         {
             LogDebug("On Before", name, methodBase);
-            var sw = stopWatchPool.GetObject();
-            sw.Restart();
-
-            string thread_id = System.Threading.Thread.CurrentThread.ManagedThreadId.ToString();
-            string name_full_name = tryGetNameSpace(methodBase);
-            string stop_watch_key = $"{thread_id}#{name_full_name}#{name}";
-            StopWatchDictionary.TryAdd(stop_watch_key, sw);
-            //CallContext.SetData(CALL_CONTEXT_KEY_STOPWATCH, sw);
         }
 
         private void LogDebug(string action, string name, MethodBase methodBase)
@@ -66,36 +56,39 @@ namespace com.mirle.ibg3k0.sc.Common.AOP
         public void After([Argument(Source.Name)] string name, [Argument(Source.Metadata)] System.Reflection.MethodBase methodBase, [Argument(Source.Arguments)] object[] arguments, [Argument(Source.ReturnValue)] object returnValue)
         {
             LogDebug("On After", name, methodBase);
-            //var sw = System.Runtime.Remoting.Messaging.CallContext.GetData(CALL_CONTEXT_KEY_STOPWATCH) as Stopwatch;
-            string thread_id = System.Threading.Thread.CurrentThread.ManagedThreadId.ToString();
-            string name_full_name = tryGetNameSpace(methodBase);
-            string stop_watch_key = $"{thread_id}#{name_full_name}#{name}";
-            bool is_exist = StopWatchDictionary.TryRemove(stop_watch_key, out Stopwatch sw);
-            if (is_exist)
-            {
-                sw.Stop();
-
-                if (sw.ElapsedMilliseconds > MAX_ALLOW_PROCESS_TIME_ms)
-                {
-                    LogWarn("On After", name, methodBase, sw.ElapsedMilliseconds);
-                }
-                sw.Reset();
-                stopWatchPool.PutObject(sw);
-            }
         }
 
 
         [Advice(Kind.Around, Targets = Target.Method)]
         public object Around(
             [Argument(Source.Name)] string name,
+            [Argument(Source.Metadata)] System.Reflection.MethodBase methodBase,
             [Argument(Source.Arguments)] object[] arguments,
             [Argument(Source.Target)] Func<object[], object> target)
         {
+            var sw = stopWatchPool.GetObject();
+            sw.Restart();
+            try
+            {
+                var result = target(arguments);
+                sw.Stop();
+                if (sw.ElapsedMilliseconds > MAX_ALLOW_PROCESS_TIME_ms)
+                {
+                    LogWarn("On Around After", name, methodBase, sw.ElapsedMilliseconds);
+                }
+                return result;
 
-            var result = target(arguments);
-
-
-            return result;
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "Exception");
+                throw ex;
+            }
+            finally
+            {
+                sw.Reset();
+                stopWatchPool.PutObject(sw);
+            }
         }
 
         private class StopWatchPool
